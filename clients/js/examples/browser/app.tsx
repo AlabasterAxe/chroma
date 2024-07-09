d70ffcdb3d5bb5c7647877765cdfead75105db3e
Matthew Keller <matthew.ed.keller@gmail.com>
2024-07-09 16:36:59 -0400

    update examples

--- clients/js/examples/browser/app.tsx
+++ clients/js/examples/browser/app.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback, useEffect, useState } from "react";
 import { ChromaClient } from "../../src/ChromaClient";
-import { Collection } from "../../src/Collection";
+import { Collection } from "../../src/types";
 
 const SAMPLE_DOCUMENTS = [
   "apple",
@@ -44,43 +44,47 @@ const useDocuments = (query?: string) => {
     async (abortSignal?: AbortSignal) => {
       setIsLoading(true);
       try {
+        if (!collection) {
+          return;
+        }
+
         if (query) {
-          collection?.query({ queryTexts: query }).then((results) => {
-            if (abortSignal?.aborted) {
-              return;
-            }
-
-            const maxDistance = Math.max(...(results.distances?.[0] ?? []));
-            setDocuments(
-              results.documents[0].map((document, i) => {
-                const distance = results.distances?.[0][i] ?? 0;
-                const relativeDistance = distance / maxDistance;
-
-                return {
-                  document: document!,
-                  relativeDistance,
-                };
-              }),
-            );
+          const results = await chroma.queryRecords(collection, {
+            query,
           });
-        } else {
-          collection?.get({}).then((results) => {
-            if (abortSignal?.aborted) {
-              return;
-            }
+          if (abortSignal?.aborted) {
+            return;
+          }
 
-            setDocuments(
-              results.documents.map((document) => ({
+          const maxDistance = results.distances
+            ? Math.max(...results.distances)
+            : -Infinity;
+          setDocuments(
+            results.documents.map((document, i) => {
+              const distance = results.distances?.[i] ?? 0;
+              return {
                 document: document!,
-              })),
-            );
-          });
+                relativeDistance: distance / maxDistance,
+              };
+            })
+          );
+        } else {
+          const results = await chroma.getRecords(collection);
+          if (abortSignal?.aborted) {
+            return;
+          }
+
+          setDocuments(
+            results.documents.map((document) => ({
+              document: document!,
+            }))
+          );
         }
       } finally {
         setIsLoading(false);
       }
     },
-    [collection, query],
+    [collection, query]
   );
 
   useEffect(() => {
@@ -100,7 +104,7 @@ export function App() {
   const trimmedQuery = query.trim();
 
   const { documents, revalidate, isLoading } = useDocuments(
-    trimmedQuery === "" ? undefined : trimmedQuery,
+    trimmedQuery === "" ? undefined : trimmedQuery
   );
 
   const handleDocumentAdd = async (event: React.FormEvent<HTMLFormElement>) => {
@@ -116,9 +120,9 @@ export function App() {
 
     setIsMutating(true);
     try {
-      await collection.upsert({
-        ids: [await hashString(document)],
-        documents: [document],
+      await chroma.upsertRecords(collection, {
+        id: await hashString(document),
+        document: document,
       });
 
       await revalidate();
@@ -137,10 +141,8 @@ export function App() {
 
     setIsMutating(true);
     try {
-      await collection.upsert({
-        ids: await Promise.all(
-          SAMPLE_DOCUMENTS.map(async (d) => hashString(d)),
-        ),
+      await chroma.upsertRecords(collection, {
+        ids: await Promise.all(SAMPLE_DOCUMENTS.map((d) => hashString(d))),
         documents: SAMPLE_DOCUMENTS,
       });
 
--- clients/js/examples/node/README.md
+++ clients/js/examples/node/README.md
@@ -1,4 +1,9 @@
 ## Demo in node
 
-1. `yarn dev`
-2. visit `localhost:3000`
+1. Make sure a Chroma instance that allows resetting is running locally on port 8000 (e.g `ALLOW_RESET=TRUE chroma run`)
+1. Build the client libraries: `cd .. && pnpm build && cd -`
+1. Install the dependencies (including the local reference to the Chroma JS client): `pnpm install`
+1. Run the node server: `pnpm dev`
+1. visit `localhost:3000`
+
+The browser should print the result of a document query and you should see more detailed logging in the console.
